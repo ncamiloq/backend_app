822941aa754499f2b36f086cb0ee08ed6cba074d
ncamiloq <ncamiloq@hotmail.com>
2021-06-06 14:52:33 -0500

    update test heroku

--- src/main/java/com/backendapp/controller/MainController.java
+++ src/main/java/com/backendapp/controller/MainController.java
@@ -8,11 +8,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 
 public class MainController {
 
-	@GetMapping("/test")
+	@GetMapping("/")
 	public String test() {
 		return "hola test 2";
 	}
