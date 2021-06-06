54f1f6e9f3d02ba962c2548d6493b457ef597de6
ncamiloq <ncamiloq@hotmail.com>
2021-06-06 15:18:07 -0500

    update test heroku

--- src/main/java/com/backendapp/controller/MainController.java
+++ src/main/java/com/backendapp/controller/MainController.java
@@ -1,10 +1,7 @@
 package com.backendapp.controller;
 
-
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
