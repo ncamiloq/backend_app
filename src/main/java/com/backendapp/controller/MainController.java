26568825b7faaa53b12d12ef1b157aa84a9f6131
ncamiloq <ncamiloq@hotmail.com>
2021-06-06 14:38:41 -0500

    update test heroku

--- src/main/java/com/backendapp/controller/MainController.java
+++ src/main/java/com/backendapp/controller/MainController.java
@@ -1,26 +1,22 @@
 package com.backendapp.controller;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST })
 public class MainController {
 
-@GetMapping("test")
+	@GetMapping("/test")
+	public String test() {
+		return "hola test 2";
+	}
 
-public String test() {
-	return "hola test 2";
 	
 	
-
-}
-
 }
