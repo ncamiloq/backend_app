e5c0bb0f8a18aea047c10cd81d4a5b6c1b209684
ncamiloq <ncamiloq@hotmail.com>
2021-06-06 15:56:41 -0500

    update

--- src/main/java/com/backendapp/controller/MainController.java
+++ src/main/java/com/backendapp/controller/MainController.java
@@ -1,16 +1,16 @@
 package com.backendapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/api")
 public class MainController {
 
-	@GetMapping("/")
+	@GetMapping("/dani")
 	public String test() {
-		return "hola test 2";
+		return "hola";
 	}
 
 	
